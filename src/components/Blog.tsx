3d136b43cd77d4e0627dac31f6b0f8eb0f2567bb
sho0226 <s1f102201616@iniad.org>
2024-11-23 03:06:48 +0900

    update: blog

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -5,84 +5,24 @@ import styles from './Blog.module.css';
 const Blog = () => {
   const projects = [
     {
-      id: 1,
-      name: 'Othello',
-      description: 'Next.jsで作成したオフラインオセロ',
-      image: `${basePath}/imgs/othello.png`,
-    },
-    {
-      id: 2,
-      name: 'Minesweeper',
-      description: 'Next.jsで作成したマインスイーパー',
-      image: `${basePath}/imgs/minesweeper.png`,
-    },
-    {
-      id: 3,
-      name: 'Tetris',
-      description: 'Next.jsで作成したテトリス',
-      image: `${basePath}/imgs/tetris.png`,
-    },
-    {
-      id: 4,
-      name: 'Breaking-Blocks',
-      description: 'Next.jsで作成したブロック崩し',
-      image: `${basePath}/imgs/block.png`,
-    },
-    {
-      id: 5,
-      name: 'Chess',
-      description: 'Next.jsで作成したチェス',
-      image: `${basePath}/imgs/chess.png`,
-    },
-    {
-      id: 6,
-      name: 'LightsOut',
-      description: 'Next.jsで作成した点灯ゲーム',
-      image: `${basePath}/imgs/lightsout.png`,
-    },
-    {
-      id: 7,
-      name: 'TodoList',
-      description: 'Full-Stack TypeScriptを用いたTodoList',
-      image: `${basePath}/imgs/todo.png`,
-    },
-    {
-      id: 8,
-      name: 'AIHeadlines',
-      description: 'AIを用いて最適化されたNEWSを提供するPlatform',
-      image: `${basePath}/imgs/AIHeadlines.png`,
-    },
-    {
-      id: 9,
-      name: 'Chronicle',
-      description: '【7月_Vol.8】ハッカソン技育CAMP2024の出場作品',
-      image: `${basePath}/imgs/Chronicle.png`,
-    },
-    {
-      id: 10,
-      name: '花火大会オンライン',
-      description: '【9月_Vol.14】ハッカソン技育CAMP2024の出場作品',
-      image: `${basePath}/imgs/hanabi.png`,
-    },
-    {
-      id: 11,
+      blog: 'https://qiita.com/Sho0226/items/c6d497c6f62ff68399fa',
       name: 'ポケモン図鑑',
-      description: 'PokéAPIを用いたReact開発',
-      image: `${basePath}/imgs/pokemon.png`,
+      image: `${basePath}/imgs/pokemon_blog.png`,
     },
   ];
 
   return (
     <div className={styles.worksContainer}>
-      <h2 className={styles.title}>Works</h2>
+      <h2 className={styles.title}>Blog</h2>
       <div className={styles.projectGrid}>
         {projects.map((project) => (
-          <Link href={`/works/${project.id}?view=quests`} key={project.id} passHref>
-            <div className={styles.projectCard}>
-              <img src={project.image} alt={project.name} className={styles.projectImage} />
-              <h3 className={styles.projectTitle}>{project.name}</h3>
-              <p className={styles.projectDescription}>{project.description}</p>
-            </div>
+          <Link href={project.blog} key={project.blog} passHref legacyBehavior>
+            <a target="_blank" rel="noopener noreferrer">
+              <div className={styles.projectCard}>
+                <img src={project.image} alt={project.name} className={styles.projectImage} />
+                <h3 className={styles.projectTitle}>{project.name}</h3>
+              </div>
+            </a>
           </Link>
         ))}
       </div>
